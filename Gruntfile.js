48d8f5e2621a0a78a0b5edaeb6d565349d92728d
Antoine Richard <richard.antoine@gmail.com>
2014-11-10 16:45:01 +0100

    added a junit-style reporting

--- Gruntfile.js
+++ Gruntfile.js
@@ -52,7 +52,13 @@ module.exports = function(grunt) {
     },
 
     nodeunit: {
-      tests: ['test/*_test.js']
+      tests: ['test/*_test.js'],
+      options: {
+        reporter: 'junit',
+        reporterOptions: {
+          output: 'test_reports'
+        }
+      }
     }
 
   });
